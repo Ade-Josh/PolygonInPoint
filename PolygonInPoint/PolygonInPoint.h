525c80717757366bfc9b596ac63e2f41ce567de3
Ade Josh <adroitjosh@gmail.com>
2021-06-23 12:30:45 +0300

    Added polygon2d

--- PolygonInPoint/PolygonInPoint.h
+++ PolygonInPoint/PolygonInPoint.h
@@ -23,3 +23,20 @@ inline std::istream& operator>>(std::istream& in, point2d& point)
     in >> point.x >> point.y;
     return in;
 }
+
+struct polygon2d
+{
+    explicit polygon2d(std::vector<point2d> coords);
+
+    std::vector<point2d> points;
+    std::vector<double> angles;
+
+    void push_back(point2d tmp);
+
+    [[nodiscard]] int points_count() const;
+
+    void push_back(double angle);
+
+    friend bool operator==(const polygon2d& first, const polygon2d& second);
+};
+
